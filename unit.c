ae6eed27260c23416bda00974dcc439dd737589b
Alex Wilson <gpg@probablyfine.co.uk>
2021-03-13 16:59:02 +0000

    refactor: Break value comparison into its own function

--- unit.c
+++ unit.c
@@ -21,6 +21,45 @@ static void assert_not_null(
   }
 }
 
+static char* compare_values(int type, sqlite3_value **argv) {
+
+  if (type == SQLITE_INTEGER) {
+    int value1 = sqlite3_value_int64(argv[0]);
+    int value2 = sqlite3_value_int64(argv[1]);
+
+    if (value1 == value2) {
+        return NULL;
+    } else {
+        return sqlite3_mprintf("FAIL: %d != %d", value1, value2);
+    }
+  }
+
+  if (type == SQLITE_TEXT) {
+    char* value1 = (char*) sqlite3_value_text(argv[0]);
+    char* value2 = (char*) sqlite3_value_text(argv[1]);
+
+    if (strcmp(value1, value2) == 0) {
+        return NULL;
+    } else {
+        return sqlite3_mprintf("FAIL: %s != %s", value1, value2);
+    }
+  }
+
+  if (type == SQLITE_FLOAT) {
+    double value1 = sqlite3_value_double(argv[0]);
+    double value2 = sqlite3_value_double(argv[1]);
+
+    if (value1 == value2) {
+        return NULL;
+    } else {
+        return sqlite3_mprintf("FAIL: %f != %f", value1, value2);
+    }
+  }
+
+  return "Not implemented yet";
+
+}
+
 static void assert_null(
   sqlite3_context *context,
   int argc,
@@ -38,6 +77,8 @@ static void assert_equal(
   int argc,
   sqlite3_value **argv
 ){
+  if (sqlite3_value_type(argv[0]) == SQLITE_NULL) return;
+
   int type1 = sqlite3_value_type(argv[0]);
   int type2 = sqlite3_value_type(argv[1]);
 
@@ -46,38 +87,14 @@ static void assert_equal(
     return;
   }
 
-  if (sqlite3_value_type(argv[0]) == SQLITE_NULL) return;
+  char* message = compare_values(type1, argv);
 
-  if (type1 == SQLITE_INTEGER) {
-    int value1 = sqlite3_value_int64(argv[0]);
-    int value2 = sqlite3_value_int64(argv[1]);
-
-    if (value1 == value2) {
-        printf("PASS\n");
-    } else {
-        assertion_error(context, sqlite3_mprintf("FAIL: %d != %d", value1, value2));
-    }
-  } else if (type1 == SQLITE_TEXT) {
-    char* value1 = (char*) sqlite3_value_text(argv[0]);
-    char* value2 = (char*) sqlite3_value_text(argv[1]);
-
-    if (strcmp(value1, value2) == 0) {
-        printf("PASS\n");
-    } else {
-        assertion_error(context, sqlite3_mprintf("FAIL: %s != %s", value1, value2));
-    }
-  } else if (type1 == SQLITE_FLOAT) {
-    double value1 = sqlite3_value_double(argv[0]);
-    double value2 = sqlite3_value_double(argv[1]);
-
-    if (value1 == value2) {
-        printf("PASS\n");
-    } else {
-        assertion_error(context, sqlite3_mprintf("FAIL: %f != %f", value1, value2));
-    }
+  if (message == NULL) {
+    printf("PASS\n");
   } else {
-     sqlite3_result_error(context, "Not implemented yet", -1);
+    assertion_error(context, message);
   }
+
 }
 
 int sqlite3_unit_init(
